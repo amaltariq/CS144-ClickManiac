3964c1eb11a073a4630ef92e9ec955ead6d0fc68
Catherine <catherinema828@gmail.com>
2017-03-05 18:10:05 -0800

    correct access token

--- sdk.py
+++ sdk.py
@@ -3,7 +3,7 @@ from facebookads import objects
 
 my_app_id = '299363293779254'
 my_app_secret = '3ec97126104c50bed63580b6968659fa'
-my_access_token = 'EAACEdEose0cBAMgQZB3wMK10JoqkfoVvR91hG07dRHot7eCI8tiZBoddUjD9wkXyRtQHZAg7TZC1PIFLfZCBQMbXq7chbFe458kGIP6wbPQYJz0ZCOg8303oD9NKZAwWqzrFbkCbJ6ufkJjh7SS0OV3yB6lzbT4tGX38K6oZBCnLijptAONq6kKZB2WTlylZAqD2RX5nPZCks3GgwZDZD'
+my_access_token = 'EAAEQRPLI1TYBAL52HZBz9FukEcoeHNTjaQP3kAWz0T3AtiDKC6A1ZA0LEX3auJJGKHZCZAANwAPgNBkJQGYAFuo5hqqoJ50UWSrQdomGxWCIOItdUdlhAV5HMN6AHh5E5KYHi40TV49eleTFiujRUmIdyDIBt1CTP9mGMTC0jAZDZD'
 FacebookAdsApi.init(my_app_id, my_app_secret, my_access_token)
 
 me = objects.AdUser(fbid='me')
