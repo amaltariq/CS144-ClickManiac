8b3144b36082155c56a09ebd4ba39815e5c285ee
Catherine <catherinema828@gmail.com>
2017-03-05 17:05:01 -0800

    adding quotes

--- sdk.py
+++ sdk.py
@@ -3,5 +3,5 @@ from facebookads import objects
 
 my_app_id = '299363293779254'
 my_app_secret = '3ec97126104c50bed63580b6968659fa'
-my_access_token = EAAEQRPLI1TYBABKAExXD7SpYmXxXQlnxKIveystMsZAj3q5lsjdt6MfpgaR32Akdf1MjxrAAyvKjtgU8KOtVLZA
+my_access_token = 'EAAEQRPLI1TYBABKAExXD7SpYmXxXQlnxKIveystMsZAj3q5lsjdt6MfpgaR32Akdf1MjxrAAyvKjtgU8KOtVLZA'
 FacebookAdsApi.init(my_app_id, my_app_secret, my_access_token)
